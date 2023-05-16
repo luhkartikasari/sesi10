cd6fef30b32cab19f863a94bf5f15f7563e921d5
luhkartikasari <127809285+luhkartikasari@users.noreply.github.com>
2023-05-16 12:16:43 +0800

    Add files via upload

---
+++ latihan01.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Form 1</title>
+</head>
+<body>
+    <form name="Latihan1" onsubmit="return checkform(this)">
+            <div>
+                NIM
+                <input type="text" name="txNIM">
+            </div>
+
+            <div>
+                Jenis Kelamin
+                <input type="radio" name="txJK" value="L">Laki-Laki
+                <input type="radio" name="txJK" value="P">Perempuan
+            </div>
+
+            <div>
+                Jurusan
+                <select name="txJURUSAN">
+                    <option value="MTI">MTI</option>
+                    <option value="KAB">KAB</option>
+                    <option value="DKV">DKV</option>
+                </select>
+            </div>
+
+            <div>
+                Hobby
+                <input type="checkbox" name="txhobbybola" value="1">Sepak Bola
+                <input type="checkbox" name="txhobbybasket" value="2">Bola Basket
+                <input type="checkbox" name="txhobbyberenang" value="3">Berenang
+            </div>
+
+            <div>
+                <button type="submit"> Kirim Data </button>
+            </div>
+        </form>
+
+        <script>
+            function checkform(frm){
+                let form = frm.elements;
+                let NIM = form.namedItem("txNIM").value;
+                let jnskel = form.namedItem("txJK").value;
+                let jur = form.namedItem("txJURUSAN").value;
+                let hobby = [
+                    form.namedItem("txhobbybola").checked,
+                    form.namedItem("txhobbybasket").checked,
+                    form.namedItem("txhobbyberenang").checked
+                ];
+                console.log("NIM: "+NIM);
+                console.log("Jenis Kelamin: "+jnskel);
+                console.log("Jurusan: "+jur);
+                console.log("Hobby: "+hobby);
+                return false;
+            }
+        </script>
+</body>
+</html>
